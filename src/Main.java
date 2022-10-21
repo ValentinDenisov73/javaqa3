b473947852c37d9c13d8d3568d2161ca64392ed8
Valentin Denisov <valentindenisov1989@gmail.com>
2022-10-21 15:32:16 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 9999;
+        int bonus = 20;
+        int mile = cost / bonus;
+        System.out.println("Начислено миль: " + mile);
+    }
+}
